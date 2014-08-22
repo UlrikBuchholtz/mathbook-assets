b75209e39b84b7aa9c2ad7c425c758a83d682166
Michael DuBois <michael@michael-dubois.com>
2014-08-22 17:05:20 -0400

    Fixed generate.sh so that it creates the output directory if it doesn't exist. Added comments.

--- scripts/generate.sh
+++ scripts/generate.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
+# Generates the current state of the repo into the given output directory 
+################################################################################
+
 output=$(readlink -m $1)
 repodir="$(cd $DIR; git rev-parse --show-toplevel)"
 
 cd $repodir 
 echo Generating from $repodir
-ls $repodir
+mkdir -p $output
 cp -v -r $repodir/js $output
 cp -v -r $repodir/stylesheets $output
 cp -v -r $repodir/images $output
